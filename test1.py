c811a55ce950373f48bcaf71ae43d188c56f22c1
lrosique <lambertrosique@hotmail.com>
2019-12-15 22:31:36 +0100

    init

---
+++ test1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 22:04:27 2019
+
+@author: Pensée Artificielle (www.penseeartificielle.fr)
+"""
+
+#.py file to test on colab
+import numpy as np
+
+print("file loaded")
+
+def myfunc(a):
+    print(myfunc)
+    return a+"_test1"
+
+def npfunc():
+    return np.ones(10)
+
